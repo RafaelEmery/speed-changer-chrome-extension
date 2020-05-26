6cfb90b825f815ba505a4123085f2dd69cab0834
RafaelEmery <rafael.emerycade@gmail.com>
2020-05-26 19:21:11 -0300

    Comments on content

--- src/content.js
+++ src/content.js
@@ -1,16 +1,21 @@
 console.log("Funcionando!")
 
 const intervalo = setInterval(() => {
+    
+    //Atribui a div superior id _3auIg
     const header = document.querySelector("._3auIg")
-
+    
     if (header) {
         clearInterval(intervalo)
 
         const botao = document.createElement("button")
         var htmlBotao = ">>>"
 
+        //Insere o botao em header
         botao.innerHTML = htmlBotao
         botao.classList.add("adiantarButton")
+
+        //Dispara o evento no botao
         botao.addEventListener("click", () => {
             const audios = document.querySelectorAll("audio")
 
